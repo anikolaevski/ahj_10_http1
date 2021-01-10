83855e05e9794e4e9b96ce1c0542ab9860040c1b
Alexey Nikolaevskiy <anikolaevski@gmail.com>
2021-01-10 22:24:41 +0300

    created

---
+++ src/js/server.js
@@ -0,0 +1,58 @@
+const http = require('http');
+const Koa = require('koa');
+const koaBody = require('koa-body');
+const { Ticket } = require('./ticket');
+
+const app = new Koa();
+const tickets = [];
+
+app.use(koaBody({
+  urlencoded: true,
+}));
+
+app.use(async (ctx) => {
+  // const { method } = ctx.request.querystring;
+  const { body } = ctx.request;
+  const methodArr = ctx.request.url.split('&');
+  const method = decodeURIComponent(ctx.request.url).replace('/?method=', '');
+  // eslint-disable-next-line no-console
+  console.log(`method=${method}`);
+
+  switch (method) {
+    case 'allTickets':
+      ctx.response.set({ 'Access-Control-Allow-Origin': '*' });
+      ctx.response.body = tickets;
+      // eslint-disable-next-line no-console
+      // console.log('allTickets');
+      return;
+      // eslint-disable-next-line indent
+    // TODO: обработка остальных методов
+
+    case 'ticketById':
+      // eslint-disable-next-line no-case-declarations
+      const id = methodArr[1].replace('id=', '');
+      // eslint-disable-next-line no-console
+      console.log(id);
+      return;
+
+    case 'createTicket':
+      // eslint-disable-next-line no-console
+      console.log(body);
+      ctx.response.set({ 'Access-Control-Allow-Origin': '*' });
+      // eslint-disable-next-line no-case-declarations
+      const ticket = new Ticket({
+        name: body.name,
+        description: body.description,
+      });
+      tickets.push(ticket);
+      ctx.response.body = ticket;
+      return;
+
+    // eslint-disable-next-line no-fallthrough
+    default:
+      ctx.response.status = 404;
+  }
+});
+
+// eslint-disable-next-line no-unused-vars
+const server = http.createServer(app.callback()).listen(7070);
